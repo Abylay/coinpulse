4cc505fb700c1677a66059d42a4517ca43a34481
Abylay <boranbayev.abylay@gmail.com>
2026-05-19 23:51:24 +0500

    chore: configure Prettier and ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 export default function App() {
   return (
     <div className={`flex min-h-screen items-center justify-center`}>
-      <h1 className={`text-5xl font-bold`}>
-        🪙 CoinPulse
-      </h1>
+      <h1 className={`text-5xl font-bold`}>🪙 CoinPulse</h1>
     </div>
-  )
+  );
 }
