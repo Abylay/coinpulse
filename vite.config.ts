fcda7a1d662260da1d04511603dde7d22f31b8c7
Abylay <boranbayev.abylay@gmail.com>
2026-05-19 22:25:48 +0500

    feat: configure project architecture and aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,6 +8,12 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   server: {
     allowedHosts: true,
